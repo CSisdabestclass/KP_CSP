874780be18261131eb0f676c436c0404aa3f5848
UCAS-CSisdabestclass <kristian.properzi@ucas-edu.net>
2026-09-21 13:41:43 -0600

    Madlib

---
+++ silly_sentence.py
@@ -0,0 +1,17 @@
+# KP, Mad Lib
+
+print('Madlib!')
+animal = input('Please tell me an animal:')
+adjective = input('Please tell me an adjective:')
+verb = input('Please tell me a verb (past tense):')
+adjective_1 = input('Please tell me an adjective:')
+adjective_3 = input('Please tell me an adjective:')
+size = input('Please give me a size:')
+adjective_4 = input('Please tell me an adjective:')
+adjective_5 = input('Please tell me an adjective:')
+noun = input("Please give me a noun:")
+adjective_6 = input('Please tell me an adjective:')
+animal_1 = adjective_4 + " " + animal
+animal_2 = adjective_5 + " " + noun
+animal_3 = adjective_6 + " " + animal
+print("I had a pet "+animal+" which had a very "+adjective+" illness. I "+verb+" all the way to the "+adjective_1+" doctor. When I got to the "+adjective_3+" doctor, he told me that my "+animal_1+" was in "+size+" danger! He told me to take this "+animal_2+" to help my "+animal_3+". My "+animal+" died the next day.")
